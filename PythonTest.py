6588a1fc8487d75ac113e81c19d88ce22a3158cb
Pvboni <162942016+Pvboni@users.noreply.github.com>
2024-03-27 18:02:31 -0300

    Update PythonTest.py

--- PythonTest.py
+++ PythonTest.py
@@ -1,5 +1,6 @@
 import feedparser
 import google.generativeai as genai
+from datetime import datetime, timedelta
 
 # Define the API Key for Gemini
 API_KEY = "AIzaSyANhQXJDd-PLX94-CqaVlprs8qG9_Slzq0"
@@ -8,12 +9,17 @@ genai.configure(api_key=API_KEY)
 def fetch_latest_news_rss(url):
     news_feed = feedparser.parse(url)
     latest_news = []
+    two_days_ago = datetime.now() - timedelta(days=2)  # Obtém a data de dois dias atrás
     if 'entries' in news_feed:
-        for entry in news_feed.entries[:10]:  # Limitando para os últimos 10 artigos
-            title = entry.title.strip()
-            link = entry.link
-            content = entry.get('summary', '')  # Obtendo o conteúdo do artigo
-            latest_news.append({'title': title, 'link': link, 'content': content})
+        for entry in news_feed.entries:
+            # Converte a data da entrada para um objeto datetime
+            entry_date = datetime.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %z")
+            # Verifica se a entrada é dos últimos dois dias
+            if entry_date >= two_days_ago:
+                title = entry.title.strip()
+                link = entry.link
+                content = entry.get('summary', '')  # Obtendo o conteúdo do artigo
+                latest_news.append({'title': title, 'link': link, 'content': content})
     return latest_news
 
 def categorize_articles_with_gemini_api(articles):
@@ -24,9 +30,9 @@ def categorize_articles_with_gemini_api(articles):
         content = article['content']
         category = categorize_content_with_gemini_api(content)
         if category in categorized_articles:
-            categorized_articles[category].append(article)
+            categorized_articles[category].append({'title': title, 'link': article['link']})
         else:
-            categorized_articles[category] = [article]
+            categorized_articles[category] = [{'title': title, 'link': article['link']}]
     
     return categorized_articles
 
@@ -69,5 +75,5 @@ if __name__ == "__main__":
     for category, articles in categorized_articles.items():
         print(f"{category.capitalize()}:")
         for article in articles:
-            print(article['title'])
+            print(f"Titulo: {article['title']}, Link: {article['link']}")
         print()
